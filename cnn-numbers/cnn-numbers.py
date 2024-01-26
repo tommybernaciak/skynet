cc7ccc6382c388a0b28f6e24c645129416259803
Tommy Bernaciak <contact@tommybernaciak.com>
2024-01-26 06:58:23 +0100

    reorganized code

--- cnn-numbers/cnn-numbers.py
+++ cnn-numbers/cnn-numbers.py
@@ -1,7 +1,8 @@
 # Importing the libraries
-import tensorflow as tf
 import keras as ks
+import tensorflow as tf
 from keras.datasets import mnist
+
 # from keras.preprocessing.image import ImageDataGenerator
 from matplotlib import pyplot as plt
 
@@ -13,23 +14,27 @@ print(ks.__version__)
 
 # plot first few images
 for i in range(9):
-	plt.subplot(330 + 1 + i)
-	plt.imshow(trainX[i], cmap=plt.get_cmap('gray'))
+    plt.subplot(330 + 1 + i)
+    plt.imshow(trainX[i], cmap=plt.get_cmap("gray"))
 plt.show()
 
 # Building the CNN
 
 cnn = ks.models.Sequential()
 # Convolution layer
-cnn.add(ks.layers.Conv2D(filters=32, kernel_size=3, activation='relu', input_shape=[64, 64, 3]))
+cnn.add(
+    ks.layers.Conv2D(
+        filters=32, kernel_size=3, activation="relu", input_shape=[64, 64, 3]
+    )
+)
 # Pooling
 cnn.add(ks.layers.MaxPool2D(pool_size=2, strides=2))
 # Convolution layer
-cnn.add(ks.layers.Conv2D(filters=32, kernel_size=3, activation='relu'))
+cnn.add(ks.layers.Conv2D(filters=32, kernel_size=3, activation="relu"))
 cnn.add(ks.layers.MaxPool2D(pool_size=2, strides=2))
 # Flattening
 cnn.add(ks.layers.Flatten())
 # Full Connection
-cnn.add(ks.layers.Dense(units=128, activation='relu'))
+cnn.add(ks.layers.Dense(units=128, activation="relu"))
 # Output Layer
-cnn.add(ks.layers.Dense(units=1, activation='sigmoid'))
+cnn.add(ks.layers.Dense(units=1, activation="sigmoid"))
